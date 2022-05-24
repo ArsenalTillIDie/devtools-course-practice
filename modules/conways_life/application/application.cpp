b4757d2e27a9e2235ece7f7bca7214f7d2b915bf
Орлов Максим Денисович <orlov.maxim2001@gmail.com>
2022-05-24 18:30:21 +0300

    Added application for Conway's Game of Life module

---
+++ modules/conways_life/application/application.cpp
@@ -0,0 +1,11 @@
+// Copyright 2022 Maksim Orlov
+
+#include <iostream>
+#include "../include/conways_life_app.h"
+
+int main(int argc, const char** argv) {
+	Application appl;
+	std::string res = appl(argc, argv);
+	std::cout << res << std::endl;
+	return 0;
+}
